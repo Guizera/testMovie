36a37fbbf0d6aff2718d7b0b9530387d46f10f03
Guilherme Cunha <guizera7@gmail.com>
2018-03-05 12:55:18 -0300

    Initial Commit

---
+++ testMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testMovie
+//
+//  Created by José Guilherme Alves da Cunha on 05/03/2018.
+//  Copyright © 2018 José Guilherme Alves da Cunha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
